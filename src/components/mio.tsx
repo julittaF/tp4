086c4f07a340356c59dd0cd9f521bf386bdbf154
julittaF <juli.feltan@gmail.com>
2024-06-03 21:16:43 -0300

    ante todo lo estetico

--- src/components/mio.tsx
+++ src/components/mio.tsx
@@ -2,23 +2,23 @@
 import { useGetValor, incrementValor } from '@/queries/numero';
 import React from 'react';
 import { useQuery } from '@tanstack/react-query';
-
-
 const ComponentName: React.FC = () => {
-    const { data: valor} = useQuery(useGetValor);
+    const { data: valor } = useQuery(useGetValor);
     console.log(valor?.valor);
-    const {mutate: increment} = incrementValor();
+    const { mutate: increment } = incrementValor();
     const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
         event.preventDefault();
         increment();
     };
     return (
-        <div>
-            {valor?.valor}
-            <button onClick={handleClick}>
-            Increment
-        </button>
-        </div>
+        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '98vh', backgroundColor: '#f0f0f0' }}>
+    <div style={{ fontSize: '2em', color: '#333', marginBottom: '0.5em' }}>
+        {valor?.valor}
+    </div>
+    <button style={{ padding: '0.5em 1em', fontSize: '1em', color: '#fff', backgroundColor: '#2d2525', border: 'none', borderRadius: '5px', cursor: 'pointer' }} onClick={handleClick}>
+        Increment
+    </button>
+</div>
     );
 };
 
